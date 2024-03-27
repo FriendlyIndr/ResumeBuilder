cbbde31171dce77e394a14483ae8be1d42abdd6e
Abhyudaya Tiwari <130074794+FriendlyIndr@users.noreply.github.com>
2024-03-27 14:23:06 +0530

    Update ResumeBuilderApplication.java

--- backend/src/main/java/com/example/resume_builder/ResumeBuilderApplication.java
+++ backend/src/main/java/com/example/resume_builder/ResumeBuilderApplication.java
@@ -3,7 +3,7 @@ package com.example.resume_builder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class ResumeBuilderApplication {
 
 	public static void main(String[] args) {
