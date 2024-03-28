4c2f434fbaee6eb7e519051f1aeaac9925c52f38
Abhyudaya Tiwari <130074794+FriendlyIndr@users.noreply.github.com>
2024-03-28 15:31:19 +0530

    Update Education.java

--- backend/src/main/java/com/example/resume_builder/model/Education.java
+++ backend/src/main/java/com/example/resume_builder/model/Education.java
@@ -1,8 +1,13 @@
 package com.example.resume_builder.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class Education {
+    @JsonProperty("deg_name")
     private String deg_name;
+    @JsonProperty("college_name")
     private String college_name;
+    @JsonProperty("period")
     private String period;
 
     public String getDeg_name() {
